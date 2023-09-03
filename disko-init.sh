dc84b511c4e6bd6417984f66f46ffa88004cb46c
Nikolay Kolev <nikolay@users.noreply.github.com>
2023-09-03 16:15:45 -0700

    feat: use sudo

--- disko-init.sh
+++ disko-init.sh
@@ -4,14 +4,14 @@ if [ ! -d /mnt ]; then
     sudo nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./zfs-on-nix-test-main/disko-config.nix
 fi
 
-if [ ! -d /mnt ]; then
+if [ ! -d /mnt/etc ]; then
     echo "fatal: /mnt does not exist" 2>&1
     exit 1
 fi
 
-nixos-generate-config --no-filesystems --root /mnt
+sudo nixos-generate-config --no-filesystems --root /mnt
 
-gzip < /mnt/nixos/configuration.nix | curl -s http://0paste.com/pastes.txt -F "paste[is_private]=1" -F "paste[paste_gzip]=<-"
-gzip < /mnt/nixos/hardware-configuration.nix | curl -s http://0paste.com/pastes.txt -F "paste[is_private]=1" -F "paste[paste_gzip]=<-"
+curl -F 'sprunge=/mnt/nixos/configuration.nix' http://sprunge.us
+curl -F 'sprunge=/mnt/nixos/hardware-configuration.nix' http://sprunge.us
 
-cp ./zfs-on-nix-test-main/disko-config.nix /mnt/etc/nixos
+sudo cp ./zfs-on-nix-test-main/disko-config.nix /mnt/etc/nixos
