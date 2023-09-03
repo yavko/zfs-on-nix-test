35c5f5e7757205409d6bbc2c3008f84af550a22d
Nikolay Kolev <nikolay@users.noreply.github.com>
2023-09-03 15:39:19 -0700

    fix: correct the path

--- disko-init.sh
+++ disko-init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko-config.nix --arg disks '[ "/dev/sda" "/dev/sdb" "/dev/sdc" ]'
+sudo nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./zfs-on-nix-test/disko-config.nix --arg disks '[ "/dev/sda" "/dev/sdb" "/dev/sdc" ]'
