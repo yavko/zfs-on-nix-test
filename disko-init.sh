34a242998f22b4b87baf83c0fcf42d5c8d5e8680
Nikolay Kolev <nikolay@users.noreply.github.com>
2023-09-03 16:20:03 -0700

    fix: proper curl usage

--- disko-init.sh
+++ disko-init.sh
@@ -11,7 +11,7 @@ fi
 
 sudo nixos-generate-config --no-filesystems --root /mnt
 
-curl -F 'sprunge=/mnt/etc/nixos/configuration.nix' http://sprunge.us
-curl -F 'sprunge=/mnt/etc/nixos/hardware-configuration.nix' http://sprunge.us
+curl -F 'sprunge=<-' http://sprunge.us < /mnt/etc/nixos/configuration.nix
+curl -F 'sprunge=<-' http://sprunge.us < /mnt/etc/nixos/hardware-configuration.nix
 
 sudo cp ./zfs-on-nix-test-main/disko-config.nix /mnt/etc/nixos
