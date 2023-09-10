69f277c90f8d3555d1e5f2583e2feb85b69f8583
Nikolay Kolev <nikolay@users.noreply.github.com>
2023-09-09 22:01:23 -0700

    fix: disko debugging

--- disko-config.nix
+++ disko-config.nix
@@ -11,7 +11,7 @@
         type =
           if (disk == (builtins.elemAt disks 0))
           then "gpt"
-          else "filesystem";
+          else "table";
         partitions = lib.traceVal (
           {
             zfs = {
