75092a6d2c039e567b02e354704c796725fa4e4f
yavko <yavornkolev@gmail.com>
2023-10-03 22:53:55 -0700

    change bad paths

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ -z "${SUDO_USER}" ]; then
 fi
 
 if [ ! -d /mnt ]; then
-  nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./zfs-on-nix-test-main/disko-config.nix
+  nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko-config.nix
 fi
 
 if [ ! -d /mnt ]; then
@@ -24,8 +24,8 @@ if [ ! -d /mnt/etc/nixos ]; then
 #curl -F 'sprunge=<-' http://sprunge.us < /mnt/etc/nixos/configuration.nix
 
   cp \
-    ./zfs-on-nix-test-main/configuration.nix \
-    ./zfs-on-nix-test-main/disko-config.nix \
+    ./configuration.nix \
+    ./disko-config.nix \
     /mnt/etc/nixos
 
   nixos-install
